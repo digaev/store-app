fab0780a43dedd88f015dcfd689e505ff2ab3475
Nikolay Digaev <ffs.cmp@gmail.com>
2017-07-27 09:48:51 +0500

    Better product description validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,6 @@ class Product < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
-  validates :description, presence: true, length: { maximum: 150 }
+  validates :description, presence: true, length: { in: 10..150 }
   validates :qty, presence: true, numericality: { greater_than_or_equal_to: 1 }
 end
